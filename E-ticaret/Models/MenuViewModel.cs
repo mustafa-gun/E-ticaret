d3ef93213021b950b7c43bc354375335275fe966
mustafa-gun <mustafagun.gs@gmail.com>
2022-08-08 13:31:58 +0300

    .

--- E-ticaret/Models/MenuViewModel.cs
+++ E-ticaret/Models/MenuViewModel.cs
@@ -1,8 +1,8 @@
-﻿namespace E_ticaret.Models
-{
-    public class MenuViewModel
-    {
-        //public List<Menu> AnaMenu { get; set; }
-        public List<AltMenu> AltMenu { get; set; }
-    }
-}
+﻿//namespace E_ticaret.Models
+//{
+//    public class MenuViewModel
+//    {
+//        //public List<Menu> AnaMenu { get; set; }
+//        public List<AltMenu> AltMenu { get; set; }
+//    }
+//}
